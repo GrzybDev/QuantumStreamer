ec92fd250729ad400bfca77702e2995608dc705e
Marek Grzyb <marek@grzyb.dev>
2025-06-27 18:58:25 +0200

    Reorganized project structure

--- src/pch.hpp
+++ src/pch.hpp
@@ -1,4 +1,4 @@
-// pch.h: This is a precompiled header file.
+// pch.hpp: This is a precompiled header file.
 // Files listed below are compiled only once, improving build performance for future builds.
 // This also affects IntelliSense performance, including code completion and many code browsing features.
 // However, files listed here are ALL re-compiled if any one of them is updated between builds.
@@ -8,6 +8,6 @@
 #define PCH_H
 
 // add headers that you want to pre-compile here
-#include "framework.h"
+#include "framework.hpp"
 
 #endif //PCH_H
