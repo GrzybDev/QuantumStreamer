40fac701e775144b39ba4c8648b60d66401f62a5
Marek Grzyb <marek@grzyb.dev>
2025-06-28 12:04:03 +0200

    Added version constants

--- src/framework.hpp
+++ src/framework.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+static constexpr unsigned char VERSION_MAJOR = 0;
+static constexpr unsigned char VERSION_MINOR = 1;
+static constexpr unsigned char VERSION_PATCH = 0;
+
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 // Windows Header Files
