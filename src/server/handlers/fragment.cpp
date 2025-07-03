5b014af7b75079a58651727d3d4d35d79a5d6d1f
Marek Grzyb <marek@grzyb.dev>
2025-07-03 22:05:04 +0200

    Added offline support for fragment handler

--- src/server/handlers/fragment.cpp
+++ src/server/handlers/fragment.cpp
@@ -1,6 +1,7 @@
 #include "pch.hpp"
 #include "fragment.hpp"
 
+#include "../subsystems/offline_streaming.hpp"
 #include "../subsystems/video_list.hpp"
 
 using Poco::Logger;
@@ -40,74 +41,88 @@ void FragmentRequestHandler::handleWithLogging(HTTPServerRequest& request, HTTPS
 		return;
 	}
 
-	// Call the fragment URL keeping all headers, query parameters, and body intact
-	// The only thing we need to change is the Host header to the fragment URL's host
+	OfflineStreaming& offlineStreaming = app.getSubsystem<OfflineStreaming>();
+	std::string localFragment = offlineStreaming.getLocalFragment(episode_id_, type_, bitrate_, start_time_);
 
-	// Parse fragment URL to extract the host
-	URI uri(fragmentUrl);
-	const std::string& fragmentHost = uri.getHost();
-
-	try
+	if (localFragment.empty())
 	{
-		logger.trace("Fetching fragment from remote server (%s)...", fragmentUrl);
-		HTTPRequest fragmentRequest(HTTPRequest::HTTP_GET, fragmentUrl, HTTPMessage::HTTP_1_1);
+		// Call the fragment URL keeping all headers, query parameters, and body intact
+		// The only thing we need to change is the Host header to the fragment URL's host
 
-		// Copy headers from the original request to the fragment request
-		for (const auto& [key, value] : request)
+		// Parse fragment URL to extract the host
+		URI uri(fragmentUrl);
+		const std::string& fragmentHost = uri.getHost();
+
+		try
 		{
-			if (key != "Host")
+			logger.trace("Fetching fragment from remote server (%s)...", fragmentUrl);
+			HTTPRequest fragmentRequest(HTTPRequest::HTTP_GET, fragmentUrl, HTTPMessage::HTTP_1_1);
+
+			// Copy headers from the original request to the fragment request
+			for (const auto& [key, value] : request)
 			{
-				// Skip Host header, we'll set it later
-				fragmentRequest.set(key, value);
+				if (key != "Host")
+				{
+					// Skip Host header, we'll set it later
+					fragmentRequest.set(key, value);
+				}
 			}
+
+			// Set the Host header to the fragment URL's host
+			fragmentRequest.set("Host", fragmentHost);
+
+			// Send the request to the fragment URL
+			HTTPClientSession session(uri.getHost(), uri.getPort());
+
+			// Set a timeout for the request
+			session.setTimeout(Timespan(REMOTE_TIMEOUT, 0));
+
+			// Send the request and get the response
+			session.sendRequest(fragmentRequest);
+
+			HTTPResponse fragmentResponse;
+			std::istream& fragmentResponseStream = session.receiveResponse(fragmentResponse);
+
+			std::ostringstream buffer;
+			StreamCopier::copyStream(fragmentResponseStream, buffer);
+			std::string bodyStr = buffer.str();
+
+			auto responseStatus = fragmentResponse.getStatus();
+
+			if (responseStatus != HTTPResponse::HTTP_OK)
+			{
+				logger.error("Failed to fetch fragment! Remote server returned %s status code.",
+				             std::to_string(responseStatus));
+				logger.trace(bodyStr);
+			}
+
+			response.setStatusAndReason(responseStatus);
+
+			for (const auto& [key, value] : fragmentResponse)
+				response.set(key, value);
+
+			std::ostream& responseBody = response.send();
+			responseBody.write(bodyStr.data(), static_cast<long long>(bodyStr.size()));
 		}
-
-		// Set the Host header to the fragment URL's host
-		fragmentRequest.set("Host", fragmentHost);
-
-		// Send the request to the fragment URL
-		HTTPClientSession session(uri.getHost(), uri.getPort());
-
-		// Set a timeout for the request
-		session.setTimeout(Timespan(REMOTE_TIMEOUT, 0));
-
-		// Send the request and get the response
-		session.sendRequest(fragmentRequest);
-
-		HTTPResponse fragmentResponse;
-		std::istream& fragmentResponseStream = session.receiveResponse(fragmentResponse);
-
-		std::ostringstream buffer;
-		StreamCopier::copyStream(fragmentResponseStream, buffer);
-		std::string bodyStr = buffer.str();
-
-		auto responseStatus = fragmentResponse.getStatus();
-
-		if (responseStatus != HTTPResponse::HTTP_OK)
+		catch (Poco::Exception& ex)
 		{
-			logger.error("Failed to fetch fragment! Remote server returned %s status code.",
-			             std::to_string(responseStatus));
-			logger.trace(bodyStr);
+			logger.error(
+				"An exception occurred while fetching media fragment from the remote server! [episode_id: %s, bitrate: %s, type: %s, start_time: %s] (%s)",
+				episode_id_,
+				bitrate_,
+				type_,
+				start_time_,
+				ex.displayText());
+			response.setStatusAndReason(HTTPResponse::HTTP_INTERNAL_SERVER_ERROR);
+			response.send();
 		}
-
-		response.setStatusAndReason(responseStatus);
-
-		for (const auto& [key, value] : fragmentResponse)
-			response.set(key, value);
+	}
+	else
+	{
+		logger.trace("Serving local fragment for episode %s, bitrate %s, type %s, start time %s...",
+		             episode_id_, bitrate_, type_, start_time_);
 
 		std::ostream& responseBody = response.send();
-		responseBody.write(bodyStr.data(), static_cast<long long>(bodyStr.size()));
-	}
-	catch (Poco::Exception& ex)
-	{
-		logger.error(
-			"An exception occurred while fetching media fragment from the remote server! [episode_id: %s, bitrate: %s, type: %s, start_time: %s] (%s)",
-			episode_id_,
-			bitrate_,
-			type_,
-			start_time_,
-			ex.displayText());
-		response.setStatusAndReason(HTTPResponse::HTTP_INTERNAL_SERVER_ERROR);
-		response.send();
+		responseBody.write(localFragment.data(), static_cast<long long>(localFragment.size()));
 	}
 }
